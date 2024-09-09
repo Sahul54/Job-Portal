b963463e70788ba431a3226e45c98aee59a7ea2e
sahul54 <ksahul9576@gmail.com>
2024-09-09 23:47:13 +0530

    job logic

---
+++ a.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+// float a = 30/0;
+// cout<<a;
+// char str[9] = "jdfhfkdajnvhgugujgjgbjhfytrfg";
+// return 0;
+
+if(500 <'4'){
+    cout<<"k";
+}
+else{
+    cout<<"sfd";
+}
+}
